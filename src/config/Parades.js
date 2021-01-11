dea9d6eaf7eed7bab2e997a2caa2f74efc01b881
Flavio Rocha <flavio_271295@hotmail.com>
2021-01-11 18:37:36 -0300

    remove addParede & fixed parede logic

--- src/config/Parades.js
+++ src/config/Parades.js
@@ -1,14 +1,37 @@
 import React, { useState, createContext, useContext } from 'react';
+import { QUANTIDADE_PAREDES } from './Regras'
 
 const dataContext = createContext()
 
+export const NOVA_PAREDE = (id) => {
+  return {
+    id,
+    tamanho: 0,
+    tamanhoError: false,
+    tamanhoDisponivel: 0,
+    tamanhoDisError: false,
+    comprimento: 0,
+    altura: 0,
+    alturaError: false,
+    janelas: 0,
+    janelasError: false,
+    portas: 0,
+    portasError: false,
+    tinta: 0,
+    ok: false,
+  }
+}
+
 export default function Paredes({ children }) {
 
-  const [totalParede, setTotalParede ] = useState(0)
+  const [totalParede, setTotalParede] = useState(0)
   const [paredes, setParedes] = useState([])
 
+  if (paredes.length < QUANTIDADE_PAREDES)
+    setParedes(paredes => [...paredes, NOVA_PAREDE(paredes.length)])
+
   return (
-    <dataContext.Provider value={{ paredes, setParedes, totalParede, setTotalParede}}>
+    <dataContext.Provider value={{ paredes, setParedes, totalParede, setTotalParede }}>
       {children}
     </dataContext.Provider>
   )
@@ -19,5 +42,6 @@ export function useParedeData() {
   if (!contextParede) throw new Error("useParedeData must be used within a Paredes provider");
   const { paredes, setParedes, totalParede, setTotalParede } = contextParede;
   return { paredes, setParedes, totalParede, setTotalParede }
-
 }
+
+
--- src/template/body/Body.js
+++ src/template/body/Body.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import { useParedeData } from '../../config/Parades';
-import addParede from '../../config/AddParede';
 import Card from '../card/Card';
 import Modal from '../modal/Modal';
 import './body.css';
 
 function Body() {
   const { paredes } = useParedeData();
-  addParede();
+
   return (
     <div className="body">
       {paredes.map(parede =>
